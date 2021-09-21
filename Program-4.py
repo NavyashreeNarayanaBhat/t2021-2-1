33a645e6c286cd38d6d447e848099a64b13c096e
NavyashreeNarayanaBhat <75651153+NavyashreeNarayanaBhat@users.noreply.github.com>
2021-09-21 13:39:16 +0530

    Update Program-4.py

--- Program-4.py
+++ Program-4.py
@@ -4,8 +4,7 @@ def counter(array):
         count[i] = 0
         for val in array:
             if val%i == 0:
-                count[i] += 1
-                
+                count[i] += 1    
     print(count)
 
 n = int(input("enter the number of elements "))
